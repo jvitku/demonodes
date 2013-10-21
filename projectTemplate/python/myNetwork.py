25e23e1547d3b7e8ad2fb269376e9e9adc02d646
Jaroslav Vitku <vitkujar@fel.cvut.cz>
2013-10-21 12:10:39 +0200

    demo python/myNetwork.py clened up

--- projectTemplate/python/myNetwork.py
+++ projectTemplate/python/myNetwork.py
@@ -4,57 +4,58 @@
 # by Jaroslav Vitku [vitkujar@fel.cvut.cz]
 
 import nef
-from ca.nengo.math.impl import FourierFunction
-from ca.nengo.model.impl import FunctionInput
-from ca.nengo.model import Units
-from ctu.nengoros.modules.impl import DefaultNeuralModule as NeuralModule
+#from ca.nengo.model import Units
 from ctu.nengoros.comm.nodeFactory import NodeGroup as NodeGroup
 from ctu.nengoros.comm.rosutils import RosUtils as RosUtils
+# Note that modules without outputs (Nengo-decoder) cannot use synchronous mode (DefaultNeuralModule).
+from ctu.nengoros.modules.impl import DefaultAsynNeuralModule as AsyncNeuralModule
+# The simulator waits each simulation step for the output in the synchronous mode, this case:
+from ctu.nengoros.modules.impl import DefaultNeuralModule as SyncNeuralModule
 
-# creates nef network and adds it to nengo (this must be first in the script) 
+
+################# Create Network and add it to nengo (this must be here first) 
 net=nef.Network('Publisher - Subscriber Simple Demo')
-net.add_to_nengo()  # here: delete old (toplevel) network and replace it with the newly CREATED one
+# delete old (toplevel) network and replace it with the newly CREATED one
+net.add_to_nengo()  
 
-################################## 
 ################# setup the ROS utils (optional) 
 #RosUtils.setAutorun(False)     # Do we want to autorun roscore and rxgraph? (tru by default)
 #RosUtils.prefferJroscore(True)  # preffer jroscore before the roscore? 
 
-mod  = "ctu.nengoros.comm.nodeFactory.modem.impl.DefaultModem"; # custom modem here
+mod  = "ctu.nengoros.comm.nodeFactory.modem.impl.DefaultModem"; # Use the default modem
 
-##################################
-################# create neural module which contains the publisher ROS node
-publisher = "org.hanns.myPackage.DemoPublisher";      # Java (ROS) node that does this job
+################# Create neural module which contains the publisher ROS node
+publisher = "org.hanns.myPackage.DemoPublisher"; # Java (ROS) node that does this job
 
-# # create group with a name
-g = NodeGroup("Publishing", True);        # create independent group called..
-g.addNC(publisher, "Publisher", "java");    # start java node and name it finder
-g.addNC(mod,"Modem","modem")     	        # add modem to the group
+# Create group of nodes
+# This group is represented in Nengo as one SimpleNode, can contain one or entire network of ROS nodes.
+# Independent groups are pushed into its own unique namespace (ROS comm. does not interfere)
+g = NodeGroup("Publishing", True);        		# Create independent group? => True. 
+g.addNC(publisher, "Publisher", "java");    	# start java node and name it finder
+g.addNC(mod,"Modem","modem")     	        	# add modem to the group
 g.startGroup()
 
 modem = g.getModem()
-module = NeuralModule('Publisher', modem) # construct the Neural Module
+module = SyncNeuralModule('Publisher', modem) 	# construct the Neural Module
 module.createDecoder("hanns/demo/pubsub", "int",7) # define the data link
-many=net.add(module)                    # add it into the network
+many=net.add(module)                    		# add it into the network
 
-##################################
+
 ################# create neural module which contains the publisher ROS node
 subscriber = "org.hanns.myPackage.DemoSubscriber";
 
-# create group with a name
-g2 = NodeGroup("Subscribing", True);        # create independent group called..
-g2.addNC(publisher, "Subscriber", "java");    # start java node and name it finder
-g2.addNC(mod,"Modem","modem")     	        # add modem to the group
+g2 = NodeGroup("Subscribing", True);        	
+g2.addNC(publisher, "Subscriber", "java");    	
+g2.addNC(mod,"Modem","modem")     	        	
 g2.startGroup()
 
 modem2 = g.getModem()
-module2 = NeuralModule('Subscriber', modem2) # construct the Neural Module
-module2.createEncoder("hanns/demo/pubsub", "int",7) # define the data link
-many=net.add(module2)                    # add it into the network
+module2 = AsyncNeuralModule('Subscriber', modem2)
+module2.createEncoder("hanns/demo/pubsub", "int",7)
+many=net.add(module2)                    		
 
-#################################
 ################# Wire it together
 net.connect(module.getOrigin('hanns/demo/pubsub'),module2.getTermination('hanns/demo/pubsub'))
 
-
 print 'Configuration complete.'
+
