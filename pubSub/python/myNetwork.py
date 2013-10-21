6b41d440f0c06a3c0968c07bbf192080e297d5f0
Jaroslav Vitku <jarda.vitku@gmail.com>
2013-10-21 21:28:36 +0200

    started adding demo project(s)

---
+++ pubSub/python/myNetwork.py
@@ -0,0 +1,54 @@
+# Create network with two NeuralModules. 
+#
+# First is demoPublisher (sends 7 integers) and the second one is demoSubscriber (receives 7 integers and does nothing).
+#
+# by Jaroslav Vitku [vitkujar@fel.cvut.cz]
+
+import nef
+from ctu.nengoros.comm.nodeFactory import NodeGroup as NodeGroup
+from ctu.nengoros.comm.rosutils import RosUtils as RosUtils
+# Note that modules without outputs (Nengo-decoder) cannot use synchronous mode (DefaultNeuralModule).
+from ctu.nengoros.modules.impl import DefaultAsynNeuralModule as AsyncNeuralModule
+# The simulator waits each simulation step for the output in the synchronous mode, this case:
+from ctu.nengoros.modules.impl import DefaultNeuralModule as SyncNeuralModule
+
+
+################# Create Network and add it to nengo (this must be here first) 
+net=nef.Network('Publisher - Subscriber Simple Demo')
+# Delete the old (toplevel) network and replace it with the newly CREATED one
+net.add_to_nengo()  
+
+################# setup the ROS utils (optional) 
+#RosUtils.setAutorun(False)     # Do we want to autorun roscore and rxgraph? (tru by default)
+#RosUtils.prefferJroscore(True)  # preffer jroscore before the roscore? 
+
+################# Create Neural Module which contains the publisher ROS node
+publisher = "org.hanns.myPackage.DemoPublisher";# Java (ROS) node that does this job
+
+# Create group of nodes
+# This group is represented in Nengo as one SimpleNode, can contain one or entire network of ROS nodes.
+# Independent groups are pushed into its own unique namespace (ROS comm. does not interfere)
+g = NodeGroup("Publishing", True);        		# Create independent group? => True. 
+# addNode() pass usually: what to launch (string), name, type {modem,java,native}
+g.addNode(publisher, "Publisher", "java");    	# start java node and name it finder
+module = SyncNeuralModule('Publisher', g) 	    # construct the Neural Module
+module.createDecoder("hanns/demo/pubsub", "int",7) # define the data link
+many=net.add(module)                    		# add it into the network
+
+
+################# Create Neural Module which contains the publisher ROS node
+subscriber = "org.hanns.myPackage.DemoSubscriber";
+
+g2 = NodeGroup("Subscribing", True);        	
+g2.addNode(publisher, "Subscriber", "java");    	
+
+# starts NeuralModule (nengoros term.) = SimpleNode (Nengo term.) = group of ROS nodes. Now add IO.
+module2 = AsyncNeuralModule('Subscriber', g2)   
+module2.createEncoder("hanns/demo/pubsub", "int",7)
+many=net.add(module2)
+
+################# Wire it together
+net.connect(module.getOrigin('hanns/demo/pubsub'), module2.getTermination('hanns/demo/pubsub'))
+
+print 'Configuration complete.'
+
---
+++ pubSub/src/main/java/org/hanns/demoNodes/pubsub/MinMaxInt.java
@@ -0,0 +1,88 @@
+package org.hanns.demoNodes.pubsub;
+
+import org.apache.commons.logging.Log;
+import org.ros.message.MessageListener;
+import org.ros.namespace.GraphName;
+import org.ros.node.AbstractNodeMain;
+import org.ros.node.ConnectedNode;
+import org.ros.node.topic.Publisher;
+import org.ros.node.topic.Subscriber;
+
+
+/**
+ * Receive vector of 4 floats, get min and max value, convert to Integer
+ * and return these two.
+ * 
+ * Will ROS recogniye namespace of topic?
+ * 
+ * @author Jaroslav Vitku
+ *
+ */
+public class MinMaxInt extends AbstractNodeMain {
+
+	private final java.lang.String ann2ros = "ann2rosFloatArr";
+	protected final java.lang.String ros2ann = "ros2annFloatArr";
+
+	private float min;
+	private float max;
+	
+	@Override
+	public GraphName getDefaultNodeName() { return GraphName.of("F2IPubSub"); }
+
+	@Override
+	public void onStart(final ConnectedNode connectedNode) {
+
+		final Log log = connectedNode.getLog();
+
+		// define the publisher
+		final Publisher<std_msgs.Int32MultiArray> publisher = 
+				connectedNode.newPublisher(ros2ann, std_msgs.Int32MultiArray._TYPE);
+
+		// subscribe to given topic
+		Subscriber<std_msgs.Float32MultiArray> subscriber = 
+				connectedNode.newSubscriber(ann2ros, std_msgs.Float32MultiArray._TYPE);
+		
+		subscriber.addMessageListener(new MessageListener<std_msgs.Float32MultiArray>() {
+			// print messages to console
+			@Override
+			public void onNewMessage(std_msgs.Float32MultiArray message) {
+				float[] data = message.getData();
+				//log.info("received these data: "+toAr(data));
+				min = min(data);
+				max = max(data);
+				//log.info("publishing this: min: "+(double)min+"   max: "+(double)max);
+				
+				// publish
+				std_msgs.Int32MultiArray fl = publisher.newMessage();	
+				fl.setData(new int[]{Math.round(min),Math.round(max)});								
+				publisher.publish(fl);						
+			}
+		});
+
+		log.info("HEY! Node is ready now!");
+	}
+
+	protected String toAr(float[] f){
+		String out = "";
+		for(int i=0;i<f.length; i++)
+			out = out+"  "+f[i];
+		return out;		
+	}
+	
+	private float max(float[] vals){
+		float mx = Float.MIN_VALUE;
+		for(int i=0;i<vals.length; i++)
+			if(mx<vals[i])
+				mx=vals[i];
+		return mx;
+	}
+		
+	private float min(float[] vals){
+		float min = Float.MAX_VALUE;
+		for(int i=0;i<vals.length; i++)
+			if(min>vals[i])
+				min=vals[i];
+		return min;	
+	}
+	
+}
